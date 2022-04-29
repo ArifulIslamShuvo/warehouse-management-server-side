1567e18f47a26368ed5b67547cc4fd6d5546cc54
Ariful Islam Shuvo <arifulislams063@gmail.com>
2022-04-30 01:41:11 +0600

    connected server use async function

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 //middleware
-// 
 
 app.use(cors());
 app.use(express.json());
@@ -15,11 +14,18 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eut81.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('inventory connected')
-  client.close();
-});
+async function run(){
+    try{
+        await client.connect();
+        const enventoryCollection = client.db('inventoryManagement').collection('inventory');
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) =>{
