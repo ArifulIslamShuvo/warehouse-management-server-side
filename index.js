3e0ed323a796994e5925c23d3f9571eb56bdb796
Ariful Islam Shuvo <arifulislams063@gmail.com>
2022-04-30 00:20:30 +0600

    server-side initialization and mongodb connected

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//middleware
+// 
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eut81.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('inventory connected')
+  client.close();
+});
+
+
+app.get('/', (req, res) =>{
+    res.send('enventory-management');
+});
+
+
+app.listen(port, () => {
+    console.log('listening to port', port);
+})
